4287c9a275efa0ee7a15604a59db8faf14f13109
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-11-19 08:29:08 +0100

    Try input for ip address

--- tcpKlient.py
+++ tcpKlient.py
@@ -16,7 +16,8 @@ else:
 
 while run:
     skt = socket.socket()  # Laver en socket
-    host = "192.168.240.18"  # Dette er IP-adressen for Raspberry Pi
+    host = input("What is the server ip address?")
+    #host = "192.168.240.18"  # Dette er IP-adressen for Raspberry Pi
     port = 3000
     skt.connect((host, port))
 
